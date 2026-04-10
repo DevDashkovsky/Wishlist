1317942e3ec1e5f486af31d54f15bd382304c14d
Egor Dashkovskii <e.dashkovskii1@g.nsu.ru>
2026-04-10 18:38:15 +0700

    feat: add domain structs

--- internal/domain/domain.go
+++ internal/domain/domain.go
@@ -1,1 +1,91 @@
 package domain
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type User struct {
+	ID           uuid.UUID `json:"id"`
+	Email        string    `json:"email"`
+	PasswordHash string    `json:"-"`
+	CreatedAt    time.Time `json:"created_at"`
+}
+
+type Wishlist struct {
+	ID          uuid.UUID `json:"id"`
+	UserID      uuid.UUID `json:"-"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	EventDate   string    `json:"event_date"`
+	ShareToken  string    `json:"share_token"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
+
+type WishlistWithItems struct {
+	Wishlist
+	Items []Item `json:"items"`
+}
+
+type PublicWishlist struct {
+	ID          uuid.UUID `json:"id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	EventDate   string    `json:"event_date"`
+	CreatedAt   time.Time `json:"created_at"`
+}
+
+type PublicWishlistWithItems struct {
+	PublicWishlist
+	Items []Item `json:"items"`
+}
+
+type Item struct {
+	ID          uuid.UUID `json:"id"`
+	WishlistID  uuid.UUID `json:"wishlist_id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	URL         string    `json:"url"`
+	Priority    int       `json:"priority"`
+	IsReserved  bool      `json:"is_reserved"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
+
+type RegisterInput struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type LoginInput struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type WishlistInput struct {
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	EventDate   string `json:"event_date"`
+}
+
+type WishlistPatch struct {
+	Title       *string `json:"title"`
+	Description *string `json:"description"`
+	EventDate   *string `json:"event_date"`
+}
+
+type ItemInput struct {
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	URL         string `json:"url"`
+	Priority    int    `json:"priority"`
+}
+
+type ItemPatch struct {
+	Title       *string `json:"title"`
+	Description *string `json:"description"`
+	URL         *string `json:"url"`
+	Priority    *int    `json:"priority"`
+}
