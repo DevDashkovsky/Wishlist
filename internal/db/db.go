90b49371ffd0c8404143d62e36a59311c4bab564
Egor Dashkovskii <e.dashkovskii1@g.nsu.ru>
2026-04-10 16:04:49 +0700

    feat: add db pool

--- internal/db/db.go
+++ internal/db/db.go
@@ -1,1 +1,60 @@
 package db
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/pressly/goose/v3"
+)
+
+type Pool struct {
+	pool *pgxpool.Pool
+}
+
+func Connect(ctx context.Context, databaseURL string) (*Pool, error) {
+	cfg, err := pgxpool.ParseConfig(databaseURL)
+	if err != nil {
+		return nil, fmt.Errorf("parse db config: %w", err)
+	}
+	cfg.MaxConns = 20
+
+	pool, err := pgxpool.NewWithConfig(ctx, cfg)
+	if err != nil {
+		return nil, fmt.Errorf("create pool: %w", err)
+	}
+
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+
+	if err := pool.Ping(ctx); err != nil {
+		pool.Close()
+		return nil, fmt.Errorf("ping db: %w", err)
+	}
+
+	return &Pool{pool: pool}, nil
+}
+
+func (p *Pool) Close() {
+	p.pool.Close()
+}
+
+func (p *Pool) PgxPool() *pgxpool.Pool {
+	return p.pool
+}
+
+func RunMigrations(databaseURL, migrationsDir string) error {
+	db, err := sql.Open("pgx", databaseURL)
+	if err != nil {
+		return fmt.Errorf("open db for migrations: %w", err)
+	}
+	defer func() { _ = db.Close() }()
+
+	if err := goose.Up(db, migrationsDir); err != nil {
+		return fmt.Errorf("run migrations: %w", err)
+	}
+	return nil
+}
